dca3d2d4e3e0b9e83830b4302c453d374266793a
motameducation <motamsoftware@outlook.com>
2026-06-05 14:05:37 +0100

    got rid of test file

--- test.py
+++
@@ -1,7 +0,0 @@
-def generate_trip_instructions(location):
-    print(f"Looks like you are going to {location}")
-    print(f"You can use the public subway to get to {location}")
-
-generate_trip_instructions("London")
-
-
